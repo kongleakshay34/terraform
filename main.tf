d066efca30d3ccee1bcd58dd11c180285e48238e
kongleakshay34 <kongleakshay34@gmail.com>
2025-03-26 10:58:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "raham-server"
+  }
+}
+
